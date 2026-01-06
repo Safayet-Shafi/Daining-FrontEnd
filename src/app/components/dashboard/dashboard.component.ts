308faa70c690bac54fd74028faa5be7cb077e820
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2026-01-06 23:26:47 +0600

    test

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,30 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth-service';
-
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ReactiveFormsModule,
-      RouterModule,
-      CommonModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  mealForm!: FormGroup;
 
-   constructor(
+  constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
+  ngOnInit() {
+    const today = new Date().toISOString().substring(0, 10);
+
+    this.mealForm = this.fb.group({
+      mealDate: [today],
+      breakfast: [0],
+      lunch: [0],
+      dinner: [0]
+    });
+  }
+
   logout() {
-    this.authService.logout();   // removes token
-    this.router.navigate(['/login']); 
-   
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  // When checkbox changes → convert true/false to 1/0
+  onMealChange(event: any, controlName: string) {
+    this.mealForm.get(controlName)?.setValue(event.target.checked ? 1 : 0);
+  }
+
+  submitMeal() {
+    console.log(this.mealForm.value);
+
+    // TODO: call API here
   }
 }
